50a725eb310f69819a1ad1a66196132edee45e9a
Marcos Vinicius <marcos30004347@gmail.com>
2020-11-07 16:34:41 -0300

    asda

--- build/ThirdParty/rapidjson/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcos/workspace/azgard/ThirdParty/rapidjson
-# Build directory: /home/marcos/workspace/azgard/build/ThirdParty/rapidjson
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doc")
-subdirs("example")
-subdirs("test")
